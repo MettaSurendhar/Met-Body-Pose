a5df5fbb5d77f0d872013ba6054d69fd9b2a787a
Metta Surendhar <msurendhar8815@gmail.com>
2024-01-04 22:25:48 +0530

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-vars */
+
 import './App.css';
+import React, { useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as handpose from '@tensorflow-models/handpose';
+import Webcam from 'react-webcam';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'></header>
+		</div>
+	);
 }
 
 export default App;
